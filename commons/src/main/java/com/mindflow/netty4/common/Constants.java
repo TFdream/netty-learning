cca91d47503fa623fb3ea31630e9180aff56a2fa
冯兵兵 <fengbingbing@iqianjin.com>
2021-08-27 11:24:13 +0800

    增加通用工具类

--- commons/src/main/java/com/mindflow/netty4/common/Constants.java
+++ commons/src/main/java/com/mindflow/netty4/common/Constants.java
@@ -7,7 +7,9 @@ package com.mindflow.netty4.common;
 public final class Constants {
 
 	public static final String HOST = System.getProperty("host", "127.0.0.1");
-	public static final int PORT = Integer.parseInt(System.getProperty("port", "9001"));
+
+	public static final boolean SSL = System.getProperty("ssl") != null;
+	public static final int PORT = Integer.parseInt(System.getProperty("port", SSL? "8443" : "8080"));
 
 	public static final String DELIMITER = "$#";
 
